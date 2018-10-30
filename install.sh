1c254232549a5e6dc95b45125817bfccbd96f1de
mjamilfarooq <jamil.farooq@hotmail.com>
2018-10-31 04:53:58 +1100

    previous deploy directory should also be deleted

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
+echo "script will install binary in deploy folder"
+
 if [ -d build ]; then
 	echo "clearing previous build"
 	rm -r build
+fi
+
+if [ -d deploy ]; then
+	echo "removing deploy "
+	rm -r deploy
 fi
 
 mkdir build
